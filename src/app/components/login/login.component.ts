d9eec901013238872bfc353a306b9d1dddb2e039
Tarushi <tarushiarora25@gmail.com>
2026-03-04 23:05:14 +0530

    added register button to homepage and enhanced login & register ui

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
