f62243cd1939d806c76c071438a09739e2ce6d95
Tarushi <tarushiarora25@gmail.com>
2026-03-07 01:03:02 +0530

    added backend url

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,17 +6,23 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
+  // // login url of spboot backend
+  // private apiUrl1 = '/api/auth/login';
 
-  // login url of spboot backend
-  private apiUrl1 = '/api/auth/login';
+  // // register url
+  // private apiUrl2 = '/api/auth/register';
 
-  // register url
-  private apiUrl2 = '/api/auth/register';
+  // Add this line at the top of your service class
+  private baseUrl = 'https://expense-tracker-backend-ae11.onrender.com/api';
+
+  // Update your existing variables to use the Base URL
+  private apiUrl1 = `${this.baseUrl}/auth/login`;
+  private apiUrl2 = `${this.baseUrl}/auth/register`;
 
   // to call api, to send post request
   constructor(private http: HttpClient) {}
 
-  register(userData: any): Observable<any>{
+  register(userData: any): Observable<any> {
     return this.http.post<any>(this.apiUrl2, userData);
   }
 
